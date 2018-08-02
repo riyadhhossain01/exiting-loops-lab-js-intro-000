c9f26f2f6da019befb9431cfc45a8cbfad12be0e
riyadh hossain <riyadh_h@ymail.com>
2018-08-02 19:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue){
 }
 
 function findBy(array, findFn) {
-  for (let i = 0, l = array.length; i < l; i++) {
+  for (let i = 0; i<array.length; i++) {
     if (findFn(array[i])) {
       return array[i]
     }
